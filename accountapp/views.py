446facdd5b7221f4fd3f521dbcab8200f3fd3edb
seotosna <>
2021-04-23 01:59:11 +0000

    template추가 - settings,

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_world(request):
-  return HttpResponse('hello world')
+  # return HttpResponse('hello world')
+  return render(request, 'base.html')
