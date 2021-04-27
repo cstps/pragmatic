6f5c3f20a54b1fd09e64b5e7ef9541b3be267daa
seotosna <>
2021-04-27 10:51:39 +0000

    db 데이터 모두 보여주기

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from accountapp.models import HelloWorld
 
 # Create your views here.
 def hello_world(request):
   # return HttpResponse('hello world')
+  hello_world_list = HelloWorld.objects.all()
   if request.method =="POST":
-    return render(request, 'accountapp/hello_world.html', context={'text':'POST Method'})
+    temp = request.POST.get('hello_world_input')
+    new_hello_world = HelloWorld()
+    new_hello_world.text = temp
+    new_hello_world.save()
+    return HttpResponseRedirect(reverse('accountapp:hello_world'))
   else:
-    return render(request, 'accountapp/hello_world.html', context={'text':'GET Method'})
+    return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
